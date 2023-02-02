cbbc5a17be4dbd5a0cd26c8c19f995dd61d2103b
thinh12 <bmtnt09042000@gmail.com>
2023-02-02 10:15:50 +0700

    complete header and footer

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./button.scss";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({
+  width,
+  height,
+  background,
+  // hoverBackground,
+  color,
+  border,
+  borderRadius,
+  padding,
+  children,
+  fontSize,
+  fontWeight
+}) => {
+  return (
+    <button
+      className="button"
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        background: background,
+        // hoverBackground:  hoverBackground,
+        color: color,
+        border: border,
+        borderRadius: borderRadius,
+        padding: padding,
+        fontSize: fontSize,
+        fontWeight: fontWeight
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
