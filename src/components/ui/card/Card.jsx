762c5fa98e0039425e7b90a17be9016c5bd4a919
Manh <lemanhct5b@gmail.com>
2023-02-02 08:39:26 +0700

    pull

--- src/components/ui/card/Card.jsx
+++ src/components/ui/card/Card.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import './Card.scss'
-const Card = () => {
-    return (
-        <div className="card">
-            <div className="card__image">
-                <div className="image__wrapper">
-                    <a href="" title='Người Sói Tuổi Teen: Bản Điện Ảnh'></a>
-                    <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/wAkpPm3wcHRqZl8XjUI3Y2chYq2.jpg" alt="" />
-                </div>
-
-                <div className="image__options">
-                    <i class="fa-regular fa-comment-dots"></i>
-                </div>
-            </div>
-            <div className="card__content">
-                <div className="content__consensus">
-                    <div className="outer__ring">
-                        <div className="percent">
-                            <span>85%</span>
-                        </div>
-                    </div>
-                </div>
-
-                <h2>
-                    <a href="" title='Người Sói Tuổi Teen: Bản Điện Ảnh'>Người Sói Tuổi Teen: Bản Điện Ảnh</a>
-                </h2>
-
-                <p>Jan 18, 2023</p>
-            </div>
-=======
 import React from "react";
 import { CgMoreO } from "react-icons/cg";
 import CirclePercent from "../circlePercent/CirclePercent";
@@ -40,7 +8,6 @@ const Card = ({ width, height, image, name, releaseDate }) => {
       <div className="card__image" style={{ height: `${height}px` }}>
         <div className="image__wrapper">
           <img src={image} alt="" />
->>>>>>> b442c6c56d1ff3f95b7fe41c9f798436eaafe653
         </div>
         <div className="image__options">
           <CgMoreO />
--- src/components/ui/collection/Collection.jsx
+++ src/components/ui/collection/Collection.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './collection.scss'
 
-const Collection = () => {
+const Collection = ({ collection, children }) => {
     return (
         <div className="collection__wrapper">
             <div className="collection__content">
-                <div className="collection__title">
-                    <h2>Welcome.</h2>
-                    <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
+                <div className="collection__title" style={{ fontSize: collection.style.fontSizeTitle, fontWeight: collection.style.fontWeightTitle }}>
+                    <h2>{collection.title}</h2>
+                </div>
+                <div className="collection__desc" style={{ fontSize: collection.style.fontSizeDesc, fontWeight: collection.style.fontWeightDesc }}>
+                    <h3>{collection.desc}</h3>
                 </div>
                 <div className="collection__other">
-                    <input type="text" placeholder='Search' />
+                    {children}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
